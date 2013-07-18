656d519bbeeea0edb52e28668cb1391c9b840ffe
fisheryz <fisheryz@hotmail.com>
2013-07-18 12:33:42 +0800

    fisrt commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testone::Application.config.secret_key_base = 'd6ea33556f6b3d31c29471ee685870ec72761b73384f4a5dc88756dc366ebafcbf329536a6f2f9f060ab8a952e85700d1472df15a07a0483d3cdb0663c3b58ee'
